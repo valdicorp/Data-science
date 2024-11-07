836b3759612cc75d149fdfa7fcfbcef54a328576
valdicorp <133921325+valdicorp@users.noreply.github.com>
2024-11-07 15:46:54 +0100

    Add files via upload

--- app2.py
+++ app2.py
@@ -9,7 +9,7 @@ from nltk.stem import WordNetLemmatizer
 import string
 from deep_translator import GoogleTranslator 
 # from pygame import mixer
-
+nltk.download('/root/nltk_data/wordnet')
 # import speech_recognition as sr
 # nltk.download('punkt')
 # nltk.download('wordnet')
